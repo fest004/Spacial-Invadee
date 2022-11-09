cede3d29fbe5f622ab29ff2f8db9555ba463cf1a
fest004 <114393595+fest004@users.noreply.github.com>
2022-11-09 02:26:09 +0100

    Movement V1
    Made movement in essence work
    
    TODO
    - Play around with numbers
    - Test movement with others

--- Scripts/PlayerWASD.cs
+++ Scripts/PlayerWASD.cs
@@ -5,10 +5,18 @@ public class PlayerWASD : MonoBehaviour
   //  Retrieving Rigidbody
   public Rigidbody2D RB;
 
+  // Animator variable
+  public Animator animator;
+
+  // CabbagesUpdate
+
+
   //  Speed numbers to be manipulated
   public float maxSpeed;
   public float acceleration;
   public float decceleration;
+  public float velPower;
+
   
   // Determening if player is moving or not
   int isMoving;
@@ -22,13 +30,10 @@ public class PlayerWASD : MonoBehaviour
   private Vector2 moveInput;
   //Applies acceleration, raises to set power so acceleration increases with  higher speeds
 
-  //Spritestate 
-  private bool isFacingRight;
-  private bool isFacingUp;
-  private bool isIdling;
 
   void Start() {
     RB = GetComponent<Rigidbody2D>();
+   // RB.AddForce(1000 * Vector2.left);
   }
 
   void Update() 
@@ -36,38 +41,58 @@ public class PlayerWASD : MonoBehaviour
     if (Input.GetKey(KeyCode.W)) {
       moveInput = Vector2.up; 
       isMoving = 1; 
+      animator.SetBool("isFacingUp", true);
+      animator.SetBool("isIdle", false);
     } else if (Input.GetKey(KeyCode.S)) {
-      moveInput = Vector2.down;
+      moveInput = Vector2.down; 
       isMoving = 1;
+      animator.SetBool("isFacingUp", false);
+      animator.SetBool("isIdle", false);
     } else if (Input.GetKey(KeyCode.A)) {
-      moveInput = Vector2.left;
+      moveInput = Vector2.left;    
       isMoving = 1;
+      animator.SetBool("isFacingRight", false);
+      animator.SetBool("isIdle", false);
     } else if (Input.GetKey(KeyCode.D)) {
       moveInput = Vector2.right;
       isMoving = 1;
+      animator.SetBool("isFacingRight", true);
+      animator.SetBool("isIdle", false);
     } else {
       isMoving = 0;
+      animator.SetBool("isIdle", true);
     }
   }
 
   void FixedUpdate() 
   {
-    // moveInput is either a negative value, a positive value or zero
+    // isMoving is either maxSpeed or zero, which will tell us if we are moving 
     // which will tell us wether the player wants to move and in what direction
     targetSpeed = isMoving * maxSpeed;
-    Debug.Log($"Target speed is {targetSpeed}");
+
+
     // Difference between current velocity or wanted velocity (maxSpeed or 0)
-    speedDif = targetSpeed - RB.velocity.x;
-    Debug.Log($"SpeedDif is {speedDif}");
+    // speedDif = targetSpeed - 0.5f * (RB.velocity.x + RB.velocity.y); 
+    //speedDif = targetSpeed - 0.5f*(Mathf.Abs(RB.velocity.x) + Mathf.Abs(RB.velocity.y));
+    speedDif = targetSpeed - Mathf.Sqrt(Mathf.Pow(RB.velocity.x,2) + Mathf.Pow(RB.velocity.y,2))/ (maxSpeed / 5); 
+
+
+
     // If elses targetSpeed to know if it is either 0 
     // or more, and gives it acceleration value
     accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
     
-    movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, 0.5f) * Mathf.Sign(speedDif);
+    movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+    Debug.Log($"movement is {movement}");
+    Debug.Log($"speedDif is {speedDif}");
+    Debug.Log($"moveInput is {moveInput}");
     //Debug.Log(movement);
     
-
-    RB.AddForce(movement * moveInput);
+    if (movement > 0) {
+      RB.AddForce(movement * moveInput);
+    } else {
+      RB.AddForce(movement * new Vector2(RB.velocity.x, RB.velocity.y));
+    }
   }
 }
 
