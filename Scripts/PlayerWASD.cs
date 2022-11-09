1297f04f43af6881fe8fe6c72700c699c99bcbec
fest004 <114393595+fest004@users.noreply.github.com>
2022-11-09 14:24:29 +0100

    MovementV2
    - Fixed if else statements
    - Added check to see if let go of movement keys

--- Scripts/PlayerWASD.cs
+++ Scripts/PlayerWASD.cs
@@ -8,7 +8,6 @@ public class PlayerWASD : MonoBehaviour
   // Animator variable
   public Animator animator;
 
-  // CabbagesUpdate
 
 
   //  Speed numbers to be manipulated
@@ -20,6 +19,7 @@ public class PlayerWASD : MonoBehaviour
   
   // Determening if player is moving or not
   int isMoving;
+  bool isMovingBool;
   float targetSpeed;
   // calculate difference between current velocity and desired velocity (high or low)
   float speedDif;
@@ -28,37 +28,58 @@ public class PlayerWASD : MonoBehaviour
   // Direction player is going
   float movement;
   private Vector2 moveInput;
+
   //Applies acceleration, raises to set power so acceleration increases with  higher speeds
 
 
   void Start() {
     RB = GetComponent<Rigidbody2D>();
-   // RB.AddForce(1000 * Vector2.left);
   }
+
 
   void Update() 
   {
     if (Input.GetKey(KeyCode.W)) {
-      moveInput = Vector2.up; 
+      //moveInput = Vector2.up; 
+      moveInput.y = 1; 
       isMoving = 1; 
       animator.SetBool("isFacingUp", true);
       animator.SetBool("isIdle", false);
-    } else if (Input.GetKey(KeyCode.S)) {
-      moveInput = Vector2.down; 
+    } 
+
+    if (Input.GetKey(KeyCode.S)) {
+      //moveInput = Vector2.down; 
+      moveInput.y = -1; 
       isMoving = 1;
       animator.SetBool("isFacingUp", false);
       animator.SetBool("isIdle", false);
-    } else if (Input.GetKey(KeyCode.A)) {
-      moveInput = Vector2.left;    
+    } 
+    if (Input.GetKey(KeyCode.A)) {
+      //moveInput = Vector2.left;    
+      moveInput.x = -1; 
       isMoving = 1;
       animator.SetBool("isFacingRight", false);
       animator.SetBool("isIdle", false);
-    } else if (Input.GetKey(KeyCode.D)) {
-      moveInput = Vector2.right;
+    } 
+    if (Input.GetKey(KeyCode.D)) {
+      //moveInput = Vector2.right;
+      moveInput.x = 1; 
       isMoving = 1;
       animator.SetBool("isFacingRight", true);
       animator.SetBool("isIdle", false);
-    } else {
+    } 
+
+    if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) {
+      Debug.Log("y");
+      moveInput.y = 0; 
+    }
+    if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) {
+      Debug.Log("x");
+      moveInput.x = 0; 
+    }
+
+    if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)){
+      moveInput.Set(0, 0);
       isMoving = 0;
       animator.SetBool("isIdle", true);
     }
@@ -70,12 +91,10 @@ public class PlayerWASD : MonoBehaviour
     // which will tell us wether the player wants to move and in what direction
     targetSpeed = isMoving * maxSpeed;
 
-
     // Difference between current velocity or wanted velocity (maxSpeed or 0)
     // speedDif = targetSpeed - 0.5f * (RB.velocity.x + RB.velocity.y); 
     //speedDif = targetSpeed - 0.5f*(Mathf.Abs(RB.velocity.x) + Mathf.Abs(RB.velocity.y));
-    speedDif = targetSpeed - Mathf.Sqrt(Mathf.Pow(RB.velocity.x,2) + Mathf.Pow(RB.velocity.y,2))/ (maxSpeed / 5); 
-
+    speedDif = targetSpeed - Mathf.Sqrt(Mathf.Pow(RB.velocity.x,2) + Mathf.Pow(RB.velocity.y,2))/ (maxSpeed / 8); 
 
 
     // If elses targetSpeed to know if it is either 0 
@@ -83,13 +102,13 @@ public class PlayerWASD : MonoBehaviour
     accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
     
     movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
-    Debug.Log($"movement is {movement}");
-    Debug.Log($"speedDif is {speedDif}");
-    Debug.Log($"moveInput is {moveInput}");
+    //Debug.Log($"movement is {movement}");
+    //Debug.Log($"speedDif is {speedDif}");
+    //Debug.Log($"moveInput is {moveInput}");
     //Debug.Log(movement);
     
     if (movement > 0) {
-      RB.AddForce(movement * moveInput);
+      RB.AddForce(movement * moveInput); 
     } else {
       RB.AddForce(movement * new Vector2(RB.velocity.x, RB.velocity.y));
     }
