fe750e547ac4ccc6e505415c59913e85f7d4989d
fest004 <114393595+fest004@users.noreply.github.com>
2022-11-10 11:51:49 +0100

    PlayerMovement
    Added simple animation sprites to test
    direction animation.
    
    Ordered variables between PlayerData.cs and PlayerWASD.cs for convenience and readability

---
+++ Scripts/Player/PlayerData.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+  protected Rigidbody2D RB;
+  
+
+  public void Start() 
+  {
+    RB = GetComponent<Rigidbody2D>();
+  }
+
+}
+
+public class PlayerMovement : PlayerData 
+{
+  #region Player Movement 
+  // Manipulable speed numbers
+  public float maxSpeed;
+  public float acceleration;
+  public float decceleration;
+  public float velPower;
+
+  // Determening if player is moving or not
+  protected int isMoving;
+  protected bool isMovingBool;
+  protected float targetSpeed;
+  // calculate difference between current velocity and desired velocity (high or low)
+  protected float speedDif;
+  //change acceleration rate depending on whether you are trying to accelerate or not
+  protected float accelRate;
+  // Direction player is going
+  protected float movement;
+  protected Vector2 moveInput;
+
+  #endregion
+
+  //#region Attack
+
+}
---
+++ Scripts/Player/PlayerWASD.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class PlayerWASD : PlayerMovement 
+{
+  // Animator variable
+  public Animator animator;
+
+  void Update() 
+  {
+    #region GET INPUT
+    if (Input.GetKey(KeyCode.W)) {
+      moveInput.y = 1; 
+      isMoving = 1; 
+    } 
+
+    if (Input.GetKey(KeyCode.S)) {
+      moveInput.y = -1; 
+      isMoving = 1;
+    } 
+    if (Input.GetKey(KeyCode.A)) {
+      moveInput.x = -1; 
+      isMoving = 1;
+    } 
+    if (Input.GetKey(KeyCode.D)) {
+      moveInput.x = 1; 
+      isMoving = 1;
+    } 
+    #endregion
+
+    #region Check Key Let Go
+    if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) {
+      Debug.Log("y");
+      moveInput.y = 0; 
+    }
+
+    if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) {
+      Debug.Log("x");
+      moveInput.x = 0; 
+    }
+
+    if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)){
+      moveInput.Set(0, 0);
+      isMoving = 0;
+    }
+    #endregion
+
+    animator.SetFloat("Horizontal", moveInput.x);
+    animator.SetFloat("Vertical", moveInput.y);
+    animator.SetFloat("Speed", moveInput.sqrMagnitude);
+  }
+
+  void FixedUpdate() 
+  {
+    #region Calculate speed and applying to RB
+    // isMoving is either maxSpeed or zero, which will tell us if we are moving 
+    targetSpeed = isMoving * maxSpeed;
+
+    // Difference between current velocity or wanted velocity (maxSpeed or 0)
+    speedDif = targetSpeed - Mathf.Sqrt(Mathf.Pow(RB.velocity.x,2) + Mathf.Pow(RB.velocity.y,2))/ (maxSpeed / 8); 
+
+
+    // If elses targetSpeed to know if it is either 0 or more
+    accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+    
+    movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+    
+    if (movement > 0) {
+      RB.AddForce(movement * moveInput); 
+    } else {
+      RB.AddForce(movement * new Vector2(RB.velocity.x, RB.velocity.y));
+    }
+    #endregion
+  }
+}
+
