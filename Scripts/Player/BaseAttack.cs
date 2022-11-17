a64553cf37d214ebea8e49ac440cab430a0c8c8a
fest004 <114393595+fest004@users.noreply.github.com>
2022-11-17 14:27:16 +0100

    Backup
    Backuo

--- Scripts/Player/BaseAttack.cs
+++ Scripts/Player/BaseAttack.cs
@@ -20,37 +20,18 @@ public class BaseAttack : AttackData
   void FixedUpdate() {
     if (Input.GetKey(KeyCode.Space)) {
       firerateTimer += 1;
-      if (firerateTimer >= firerate) {
-        Shoot();
-        firerateTimer = 0;
-      }
+
+    if (firerateTimer >= firerate) {
+      Shoot();
+      firerateTimer = 0;
+    }
     }
   }
 
   void Shoot() 
   {
     firePoint = firePoint == firePointLeft ? firePointRight : firePointLeft;
-
-    /*
-    if (firePoint == firePointLeft) {
-      firePoint = firePointRight;
-    } else {
-      firePoint = firePointLeft;
-    }
-    */
-    
     objectPooler.SpawnFromPool("BaseAttackBullet", firePoint.transform.position, firePoint.transform.rotation);
-    // Instantiate(baseAttackBullet, firePoint.position, firePoint.rotation);
-
-    /*
-    GameObject baseAttackBullet = ObjectPool.SharedInstance.GetPooledObject();
-      if (baseAttackBullet != null) {
-        baseAttackBullet.transform.position = firePoint.transform.position;
-        baseAttackBullet.transform.rotation = firePoint.transform.rotation;
-        baseAttackBullet.SetActive(true);
-        Debug.Log("Shooty McShootShoot");
-      }
-    */
   }
 }
 
